7476368dc8e5cb44f17928416f78bb91aa58c879
Manohar Chekka <84077005+MC140@users.noreply.github.com>
2026-09-09 23:10:21 -0400

    Bump PWA cache for card refinement

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'washradar-shell-v8';
+const CACHE = 'washradar-shell-v9';
 const scopeUrl = new URL(self.registration.scope);
 const local = (path) => new URL(path, scopeUrl).toString();
 const SHELL = [local('./'), local('offline.html'), local('favicon.svg'), local('icon-192.png'), local('icon-512.png')];
