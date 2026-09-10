921291c66125119b7d25cb98a2d9c0655f3ad4bf
Manohar Chekka <84077005+MC140@users.noreply.github.com>
2026-09-10 15:47:51 -0400

    Add public account deletion route

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,7 @@ export default function App() {
     <Route path="terms" element={<LegalPage page="terms" />} />
     <Route path="sponsored" element={<LegalPage page="sponsored" />} />
     <Route path="support" element={<LegalPage page="support" />} />
+    <Route path="account-deletion" element={<LegalPage page="accountDeletion" />} />
     <Route path="admin" element={<AdminPage />} />
     <Route path="ad-preview" element={<AdPreviewPage />} />
     <Route path="*" element={<NotFoundPage />} />
